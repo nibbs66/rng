80f1349fe9e2d019fa94aebad8ed730017a73a2e
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-06-04 12:43:08 +0200

    add Category Page

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,10 @@ import React from 'react'
 import { SessionProvider } from "next-auth/react"
 
 
-function MyApp({ Component,  pageProps }) {
+function MyApp({ Component, session,  pageProps }) {
     const getLayout = Component.getLayout || ((page) => page)
     return (
-        <SessionProvider session={pageProps.session}>
+        <SessionProvider session={session}>
             {getLayout(<Component {...pageProps} />)}
         </SessionProvider>
         )
